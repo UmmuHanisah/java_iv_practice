2e31c7217b6e2560841bfdd8fd5565d12bd7c4f7
Ummu Hanisah <ummuhanisah0625@gmail.com>
2024-03-05 12:56:15 +0800

    Refactor class name

--- src/java_iv_practice/ReversingString.java
+++ src/java_iv_practice/ReversingString.java
@@ -1,17 +1,18 @@
 package java_iv_practice;
 
 // Write a function to reverse the following string
-public class SortingString {
+public class ReversingString {
 	public static void main(String args[]) {
 
 		String originalString = "abcd";
 		String reversedString = "";
-		char oriCharAt;
+		//char oriCharAt;
 
 		for (int i = 0; i < originalString.length(); i++) {
 			reversedString = originalString.charAt(i) + reversedString;
 
-			/*
+			/* For debugging
+			 * =============
 			 * oriCharAt = originalString.charAt(i); System.out.println("Test inner " +
 			 * oriCharAt); System.out.println("Test inner " + reversedString);
 			 */
